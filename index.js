52bae6a548012884844db6c88b34a339c373ecdb
Guilherme dos Santos Vahl <gdsvahl@inf.ufpel.edu.br>
2021-11-10 16:30:06 -0300

    adding audio feature data on csv tracks files

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const spotifyApi = new SpotifyWebApi({
 })
 
 const columns = 
-    `id,name,popularity,duration_ms,explicit,track_number,type,release_date,album_name,album_total_tracks,artist_id,artist_name\n`
+    `id,name,popularity,duration_ms,explicit,track_number,type,release_date,album_name,album_total_tracks,artist_id,artist_name,acousticness,danceability,energy,instrumentalness,key,liveness,loudness,mode,speechiness,tempo,time_signature,valence\n`
 
 fs.writeFileSync(csvPath, columns, { flag: 'w+' })
 
@@ -31,26 +31,45 @@ const updateExpiryToken = async () => {
 }
 
 const retrieveTracks = async () => {
-    const { body } = await spotifyApi.searchTracks('year:2021', {
+    const { body: initialTracks } = await spotifyApi.searchTracks('year:2021', {
         offset,
         limit
     })
+    
+    const tracksIds = initialTracks.tracks.items.map(({ id }) => id)
+    
+    const { body: audioFeatures } = await spotifyApi.getAudioFeaturesForTracks(tracksIds)
 
-    const tracksObjectInfo = body.tracks.items.map(track => ({
-            id: track.id,
-            name: track.name,
-            popularity: track.popularity,
-            duration_ms: track.duration_ms,
-            explicit: track.explicit,
-            track_number: track.track_number,
-            type: track.type,
-            release_date: track.album.release_date,
-            album_name: track.album.name,
-            album_total_tracks: track.album.total_tracks,
-            artist_id: track.artists[0].id,
-            artist_name: track.artists[0].name,
-        })
-    )
+    const tracksObjectInfo = initialTracks.tracks.items.map(track => {
+        const audioFeature = audioFeatures.audio_features.find(audioFeat => audioFeat.id === track.id)
+
+        return {
+            id: track.id || '',
+            name: track.name || '',
+            popularity: track.popularity || '',
+            duration_ms: track.duration_ms || '',
+            explicit: track.explicit || '',
+            track_number: track.track_number || '',
+            type: track.type || '',
+            release_date: track.album.release_date || '',
+            album_name: track.album.name || '',
+            album_total_tracks: track.album.total_tracks || '',
+            artist_id: track.artists[0].id || '',
+            artist_name: track.artists[0].name || '',
+            acousticness: audioFeature.acousticness || '',
+            danceability: audioFeature.danceability || '',
+            energy: audioFeature.energy || '',
+            instrumentalness: audioFeature.instrumentalness || '',
+            key: audioFeature.key || '',
+            liveness: audioFeature.liveness || '',
+            loudness: audioFeature.loudness || '',
+            mode: audioFeature.mode || '',
+            speechiness: audioFeature.speechiness || '',
+            tempo: audioFeature.tempo || '',
+            time_signature: audioFeature.time_signature || '',
+            valence: audioFeature.valence || ''
+        }
+    })
 
     const tracksCsvInfo = tracksObjectInfo.reduce((acc, track) => 
         acc.concat(Object.values(track).toString(), '\n'), ''
